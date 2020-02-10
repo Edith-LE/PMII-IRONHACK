53236287ab8d1110607e70d1331120c9b0d71bd5
Edith-LE <edith.lop.estrada@gmail.com>
2020-02-09 22:01:24 -0600

    signup and login done

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const { model, Schema } = require('mongoose');
+const PLM = require ('passport-local-mongoose');
+
+const userSchema = new Schema(
+  {
+    email: String,
+    name: String,
+    googleID: String,
+  },
+  {
+    timestamps: true
+  }
+);
+
+
+
+userSchema.plugin(PLM, { usernameField: "email"});
+
+module.exports =  model ('User', userSchema);
---
+++ routes/auth-routes.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = express.Router();
+const passport = require ('../config/passport')
+
+const User = require ('../models/User')
+
+router
+.get('/signup',  (req, res) =>{
+  res.render ('auth/signup')
+});
+
+router
+.post('/signup',async (req, res) =>{
+  const { name, email, password} = req.body
+  if ( email === '' || password === "") {
+    return res.render ('auth/signup', { 
+      message: "Por favor indica el email y contraseña"})
+  }
+
+  const userOnDB =  await User.findOne({ email });
+  if (userOnDB !== null) {
+    res.render ('auth/signup', {message: "El correo  ya fue registrado"})
+  }
+  await User.register({ name, email }, password)
+  res.redirect('/login')
+}); 
+
+router
+.get ('/login', (req, res) =>{
+  res.render('auth/login', { "message": req.flash ("error")})
+});
+
+router
+.post('/login',
+passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+
+function ensureLogin(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    return res.redirect('/login');
+  }
+}
+
+router
+.get('/private-page', ensureLogin, (req, res, next)=>{
+  res.render('/private', {user: req.user});
+});
+
+router
+.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
+
+module.exports = router;
